b53abc5e36ce7c5ad2972281adce43daf5c11ce7
Leo Black <dev@n3t.run>
2025-06-24 17:07:23 -0700

    Wallet change

--- scripts/adminMint.js
+++ scripts/adminMint.js
@@ -2,7 +2,8 @@ const { ethers } = require('hardhat')
 
 async function main() {
   const proxyAddress = '0x76065074344824a3201E46b84FA6611384bD7E92'
-  const recipient = '0xfd0dF1cb53a9F4576D899C275928999fc73e78E1'
+  const recipient = '0x7b7c54EE775CB1ad6f72bEEED82c3Bb0AF789699'
+  //const recipient = '0xfd0dF1cb53a9F4576D899C275928999fc73e78E1'
   const quantity = 1
 
   const MadeForRats = await ethers.getContractFactory('MadeForRats')
