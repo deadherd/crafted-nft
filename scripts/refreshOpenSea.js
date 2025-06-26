eeb6098ac1f088862dca68630d234bda77454286
Leo Black <dev@n3t.run>
2025-06-25 18:50:09 -0700

    Refrsh md

--- scripts/refreshOpenSea.js
+++ scripts/refreshOpenSea.js
@@ -3,7 +3,7 @@ const opensea = require('@api/opensea')
 async function main() {
   const chain = 'base'
   const contractAddress = '0x76065074344824a3201E46b84FA6611384bD7E92'
-  const tokenId = '1'
+  const tokenId = '0'
 
   opensea.server('https://api.opensea.io')
 
